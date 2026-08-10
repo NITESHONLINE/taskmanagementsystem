05190eedb69543e821f2e708669e4b6b56807704
shreesant <shreesantadhikari4590@gmail.com>
2026-08-10 17:06:26 +0545

    product page created

--- src/components/Addproduct.jsx
+++ src/components/Addproduct.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Addproduct = () => {
+
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [description, setDescriptin] = useState("");
+  const [image, setImage] = useState("");
+ 
+
+  const handleSubmitProduct = (e) => {
+    e.preventDefault()
+    const singleProduct = {name, price, description, image}
+    console.log(singleProduct)
+
+  }
+ 
+
   return (
-    <div>Addproduct</div>
+    <>
+
+      <form onSubmit={handleSubmitProduct} className='shadow-md p-2 rounded-md w-[220px]'>
+        <label htmlFor="">Name</label><br />
+        <input type="text" value={name} onChange={(e)=> setName(e.target.value)} className='border' placeholder='Enter product name' /> <br />
+
+        <label htmlFor="">Price</label><br />
+        <input type="text" value={price} onChange={(e)=> setPrice(e.target.value)} className='border' placeholder='Enter product price' /> <br />
+
+        <label htmlFor="">Description</label><br />
+        <input type="text" value={description} onChange={(e)=> setDescriptin(e.target.value)} className='border' placeholder='Enter product description' /> <br />
+       
+        <label htmlFor="">Image</label><br />
+        <input type="text" value={image} onChange={(e)=> setImage(e.target.value)} className='border' placeholder='Enter product image' /> <br />
+    
+        <button>Save</button>
+      </form>
+
+
+    </>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Product from '../components/Product'
+import Addproduct from '../components/Addproduct'
 
 // import component => ctrl + spacebar 
 
@@ -20,8 +21,16 @@ const Home = () => {
         },
     ]
 
+    const singledata = {
+            name: "Orange",
+            price: 200,
+            description: "Best orange",
+    }
+
   return (
     <>
+
+    <Addproduct/>
 
     <Product data={product}/>
 
